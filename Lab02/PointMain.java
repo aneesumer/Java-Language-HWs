3e678126e822c42a5bfec91f41c28a2073e88174
Anees Umer <aneesumar72@gmail.com>
2021-12-26 00:52:57 +0100

    Update PointMain.java

--- Lab02/PointMain.java
+++ Lab02/PointMain.java
@@ -11,20 +11,4 @@ public class PointMain {
         p.mirror(2,1);
 
         System.out.println("The Point after move is: " + p.x + " " + p.y);
-
-        Point q = new Point();
-        q.x = 5;
-        q.y = 5;
-
-        System.out.println("The distance of p from q is: " + p.distance(q));
-
-        Point s = new Point();
-        s.x = 2;
-        s.y = 1;
-
-        p.mirror(s);
-
-
-        System.out.println("The Point after move is: " + p.x + " " + p.y);
-    }
 }
