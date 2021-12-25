ce7aa04d1a1e10029af3053e9d9a8aee732365f8
Anees Umer <aneesumar72@gmail.com>
2021-12-26 00:56:13 +0100

    Update Point.java

--- Lab02/Point.java
+++ Lab02/Point.java
@@ -1,4 +1,4 @@
-// Task01 
+// Task01 & Task02
 // For Main Program, go to PointMain.java
 
 public class Point {
@@ -14,3 +14,14 @@ public class Point {
             x = 2*cx -x;
             y = 2*cy -y;
         }
+    
+    /*
+        void mirror (Point p) {
+            x = 2*p.x -x;
+            y = 2*p.y -y;
+        }
+
+        double distance (Point P) {
+           return Math.sqrt((x-P.x)*(x-P.x) + (y-P.y)*(y-P.y));
+        }
+*/  
